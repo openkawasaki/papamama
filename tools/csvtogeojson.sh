39e820d8a02cd05f7aae72169361f9673cc96915
Daijiro SUENAGA <info@awks.jp>
2015-09-09 20:38:14 +0900

    Google SpeadsheetからnurseryFacilities.geojsonを作成するスクリプト追加

---
+++ tools/csvtogeojson.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+ID="1wcKzZPd8gFnORUynqZUF5WXzZilLE3wqbDF9MBJMMA4";
+BASE="https://docs.google.com/spreadsheets/d/${ID}/export?format=csv&id=${ID}&gid=";
+CMD="/Users/daijiro/Sites/csv2geojson/csv2geojson";
+GEOJSON="../data/nurseryFacilities.geojson";
+
+DIR=./csv/
+ALL=${DIR}all.csv
+TEMP=${DIR}temp.csv
+HEADER=header
+LOG=${DIR}cmd.log
+
+GIDS=(
+	"2096853833"
+	"1898655938"
+	"2884537"
+	"973798039"
+	"1334772284"
+	"1500086315"
+	"1513626499"
+	"1109938430"
+	"1357964101"
+	"1863141824"
+);
+NAMES=(
+	"kawasaki.csv"
+	"saiwai.csv"
+	"nakahara.csv"
+	"takatsu.csv"
+	"miyamae.csv"
+	"tama.csv"
+	"aso.csv"
+	"onakama.csv"
+	"nintei.csv"
+	"chiiki.csv"
+);
+
+
+mkdir -p ${DIR}
+
+for (( I = 0; I < ${#GIDS[@]}; ++I ))
+do
+	wget ${BASE}${GIDS[$I]} -O ${DIR}${NAMES[$I]}
+done
+
+rm -f ${DIR}
+for (( I = 0; I < ${#NAMES[@]}; ++I ))
+do
+	tail -n `wc -l ${DIR}${NAMES[$I]} | awk '{ print $1 }'` ${DIR}${NAMES[$I]} >> ${TEMP}
+	echo >> ${TEMP}
+done
+
+HID=1
+cp ${HEADER} ${ALL}
+cat ${TEMP} | while read line
+do
+	echo ${HID}${line} >> ${ALL}
+	HID=`expr $HID + 1`
+done
+
+
+${CMD} --lat Y --lon X  ${ALL} > ${GEOJSON} 2> ${LOG}
+if [ -s ${LOG} ];then
+	echo "error occurred. see ${LOG} file."
+fi
